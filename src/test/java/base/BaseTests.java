b6ae66e0036e98d1720bf8543edca6c69e1bb142
Dahn A <dahnadams79@gmail.com>
2026-04-11 19:58:19 -0400

    Updated yaml file and BaseTest for headless mode use

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,10 +1,12 @@
 package base;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import utils.ScreenshotUtil;
-import org.junit.jupiter.api.TestInfo;
 
 import java.time.Duration;
 
@@ -14,7 +16,17 @@ public class BaseTests {
 
     @BeforeEach
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        String ci = System.getenv("CI");
+        if ("true".equalsIgnoreCase(ci)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
